2f4396b513cd7d051997f839428bf072948d030e
ravinder <ravinder50300@gmail.com>
2025-12-10 18:41:54 +0530

    add user table

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,48 @@
+import {Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
+
+@Entity("users")
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({
+        type: "varchar",
+    })
+    name: string;
+
+    @Column({
+        type: 'varchar',
+        unique: true,
+        nullable: false,
+        length: 150,
+    })
+    email: string;
+
+    @Column({
+        type: 'varchar',
+        length: 255, // for hashed password
+        nullable: false,
+    })
+    passwordHash: string;
+
+
+    @Column({
+        type: "number",
+        nullable: true,
+    })
+    age: number;
+
+    @Column({ type: 'float', nullable: true })
+    height: number; // cm
+
+    @Column({ type: 'float', nullable: true })
+    weight: number; // kg
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+
+}
