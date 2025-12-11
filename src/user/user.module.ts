e44babf2c3c7957cc3e79fcc98d89eb653842699
ravinder <ravinder50300@gmail.com>
2025-12-11 22:12:30 +0530

    setup user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common';
+import {UserService} from "./user.service";
+import {UserController} from "./user.controller";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {User} from "./entities/user.entity";
 
-@Module({})
-export class UserModule {
-
-}
+@Module({
+    controllers: [UserController],
+    providers: [UserService],
+    imports: [
+        TypeOrmModule.forFeature([User])
+    ],
+    exports: [UserService]  // export for auth service
+})
+export class UserModule {}
